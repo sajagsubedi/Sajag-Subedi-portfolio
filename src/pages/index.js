aa04035fcff0e95277bb38225d7040b0b0d5cff7
Sajag Subedi <108583402+sajagsubedi@users.noreply.github.com>
2023-06-17 13:14:42 +0545

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,11 @@ function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="favicon.ico"
+        />
       </Head>
       <main className={styles.main}>
         <HeroSection />

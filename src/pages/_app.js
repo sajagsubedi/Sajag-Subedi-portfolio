86613eabcbe6e41214d6b93614a2e81756945f78
Sajag Subedi <108583402+sajagsubedi@users.noreply.github.com>
2023-06-17 08:48:43 +0545

    Add files via upload

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,1 +1,13 @@
+import "@/styles/globals.css";
+import { Header, ScrollTop, Footer } from "../Components/index.js";
 
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <ScrollTop/>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import {
+  HeroSection,
+  Services,
+  Skills,
+  Projects,
+  Contact,
+} from "../Components/index.js";
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sajag Subedi: Innovative Web Developer</title>
+        <meta
+          name="description"
+          content="Sajag,full stack web developer. Discover the realm of cutting-edge tech solutions and explore the services offered by this passionate and skilled professional."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <HeroSection />
+        <Services />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+    </>
+  );
+}
+export default Home;

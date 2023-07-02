cd4a566bcedce11a7a5cf1cfc0fe445516ec2828
Sajag Subedi <108583402+sajagsubedi@users.noreply.github.com>
2023-07-02 15:13:09 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
+  env: {
+NEXT_PUBLIC_KEY:process.env.NEXT_PUBLIC_KEY
+  }
 };
 
 module.exports = nextConfig;
